2132aff7a3b2b3f4da3227a9cc87ae8c8cf449fb
elonehoo <hi@elonehoo.me>
2025-11-12 14:19:41 +0800

    fix: update package.json to ensure private field is set and clean up exports in index.ts

--- packages/vue/src/index.ts
+++ packages/vue/src/index.ts
@@ -1,4 +1,4 @@
 export * from './components'
+export { useEmitAsProps, useForwardExpose, useForwardProps, useForwardPropsEmits } from './composables'
 export * from './providers'
-export { useEmitAsProps, useForwardProps, useForwardPropsEmits, useForwardExpose } from './composables'
 export type { Assign, Optional } from './types'
