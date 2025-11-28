b4872c2ff894af4caf3d31967ea6fa4c27ceba5f
elonehoo <hi@elonehoo.me>
2025-11-28 20:31:24 +0800

    fix: update test file

--- packages/vue/vitest.config.ts
+++ packages/vue/vitest.config.ts
@@ -27,7 +27,9 @@ export default defineConfig({
             enabled: true,
             headless: true,
             provider: playwright({}),
-            instances: [{ browser: 'chromium' }],
+            instances: [
+              { browser: 'chromium' },
+            ],
           },
           setupFiles: ['.storybook/vitest.setup.ts'],
         },
