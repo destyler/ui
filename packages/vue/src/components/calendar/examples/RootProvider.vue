fe55bf3fc51e16e2f6e197ad0bb63d175a8cf653
elonehoo <hi@elonehoo.me>
2026-03-04 15:23:47 +0800

    feat(calendar): implement standalone calendar styles and enhance examples with Teleport for better positioning

--- packages/vue/src/components/calendar/examples/RootProvider.vue
+++ packages/vue/src/components/calendar/examples/RootProvider.vue
@@ -11,84 +11,86 @@ const datePicker = useCalendar()
     <Calendar.Label>Label</Calendar.Label>
     <Calendar.Control>
       <Calendar.Input />
-      <Calendar.Trigger>📅</Calendar.Trigger>
+      <Calendar.Trigger />
       <Calendar.ClearTrigger>Clear</Calendar.ClearTrigger>
     </Calendar.Control>
-    <Calendar.Positioner>
-      <Calendar.Content>
-        <Calendar.YearSelect />
-        <Calendar.MonthSelect />
-        <Calendar.View view="day">
-          <Calendar.Context v-slot="api">
-            <Calendar.ViewControl>
-              <Calendar.PrevTrigger>Prev</Calendar.PrevTrigger>
-              <Calendar.ViewTrigger>
-                <Calendar.RangeText />
-              </Calendar.ViewTrigger>
-              <Calendar.NextTrigger>Next</Calendar.NextTrigger>
-            </Calendar.ViewControl>
-            <Calendar.Table>
-              <Calendar.TableHead>
-                <Calendar.TableRow>
-                  <Calendar.TableHeader v-for="(weekDay, id) in api.weekDays" :key="id">
-                    {{ weekDay.short }}
-                  </Calendar.TableHeader>
-                </Calendar.TableRow>
-              </Calendar.TableHead>
-              <Calendar.TableBody>
-                <Calendar.TableRow v-for="(week, id) in api.weeks" :key="id">
-                  <Calendar.TableCell v-for="(day, id) in week" :key="id" :value="day">
-                    <Calendar.TableCellTrigger>{{ day.day }}</Calendar.TableCellTrigger>
-                  </Calendar.TableCell>
-                </Calendar.TableRow>
-              </Calendar.TableBody>
-            </Calendar.Table>
-          </Calendar.Context>
-        </Calendar.View>
-        <Calendar.View view="month">
-          <Calendar.Context v-slot="api">
-            <Calendar.ViewControl>
-              <Calendar.PrevTrigger>Prev</Calendar.PrevTrigger>
-              <Calendar.ViewTrigger>
-                <Calendar.RangeText />
-              </Calendar.ViewTrigger>
-              <Calendar.NextTrigger>Next</Calendar.NextTrigger>
-            </Calendar.ViewControl>
-            <Calendar.Table>
-              <Calendar.TableBody>
-                <Calendar.TableRow
-                  v-for="(months, id) in api.getMonthsGrid({ columns: 4, format: 'short' })"
-                  :key="id"
-                >
-                  <Calendar.TableCell v-for="(month, id) in months" :key="id" :value="month.value">
-                    <Calendar.TableCellTrigger>{{ month.label }}</Calendar.TableCellTrigger>
-                  </Calendar.TableCell>
-                </Calendar.TableRow>
-              </Calendar.TableBody>
-            </Calendar.Table>
-          </Calendar.Context>
-        </Calendar.View>
-        <Calendar.View view="year">
-          <Calendar.Context v-slot="api">
-            <Calendar.ViewControl>
-              <Calendar.PrevTrigger>Prev</Calendar.PrevTrigger>
-              <Calendar.ViewTrigger>
-                <Calendar.RangeText />
-              </Calendar.ViewTrigger>
-              <Calendar.NextTrigger>Next</Calendar.NextTrigger>
-            </Calendar.ViewControl>
-            <Calendar.Table>
-              <Calendar.TableBody>
-                <Calendar.TableRow v-for="(years, id) in api.getYearsGrid({ columns: 4 })" :key="id">
-                  <Calendar.TableCell v-for="(year, id) in years" :key="id" :value="year.value">
-                    <Calendar.TableCellTrigger>{{ year.label }}</Calendar.TableCellTrigger>
-                  </Calendar.TableCell>
-                </Calendar.TableRow>
-              </Calendar.TableBody>
-            </Calendar.Table>
-          </Calendar.Context>
-        </Calendar.View>
-      </Calendar.Content>
-    </Calendar.Positioner>
+
+    <Teleport to="body">
+      <Calendar.Positioner class="calendar-rootprovider" data-testid="positioner">
+        <Calendar.Content>
+          <Calendar.View view="day">
+            <Calendar.Context v-slot="api">
+              <Calendar.ViewControl>
+                <Calendar.PrevTrigger>Prev</Calendar.PrevTrigger>
+                <Calendar.ViewTrigger>
+                  <Calendar.RangeText />
+                </Calendar.ViewTrigger>
+                <Calendar.NextTrigger>Next</Calendar.NextTrigger>
+              </Calendar.ViewControl>
+              <Calendar.Table>
+                <Calendar.TableHead>
+                  <Calendar.TableRow>
+                    <Calendar.TableHeader v-for="(weekDay, id) in api.weekDays" :key="id">
+                      {{ weekDay.short }}
+                    </Calendar.TableHeader>
+                  </Calendar.TableRow>
+                </Calendar.TableHead>
+                <Calendar.TableBody>
+                  <Calendar.TableRow v-for="(week, id) in api.weeks" :key="id">
+                    <Calendar.TableCell v-for="(day, id) in week" :key="id" :value="day">
+                      <Calendar.TableCellTrigger>{{ day.day }}</Calendar.TableCellTrigger>
+                    </Calendar.TableCell>
+                  </Calendar.TableRow>
+                </Calendar.TableBody>
+              </Calendar.Table>
+            </Calendar.Context>
+          </Calendar.View>
+          <Calendar.View view="month">
+            <Calendar.Context v-slot="api">
+              <Calendar.ViewControl>
+                <Calendar.PrevTrigger>Prev</Calendar.PrevTrigger>
+                <Calendar.ViewTrigger>
+                  <Calendar.RangeText />
+                </Calendar.ViewTrigger>
+                <Calendar.NextTrigger>Next</Calendar.NextTrigger>
+              </Calendar.ViewControl>
+              <Calendar.Table>
+                <Calendar.TableBody>
+                  <Calendar.TableRow
+                    v-for="(months, id) in api.getMonthsGrid({ columns: 4, format: 'short' })"
+                    :key="id"
+                  >
+                    <Calendar.TableCell v-for="(month, id) in months" :key="id" :value="month.value">
+                      <Calendar.TableCellTrigger>{{ month.label }}</Calendar.TableCellTrigger>
+                    </Calendar.TableCell>
+                  </Calendar.TableRow>
+                </Calendar.TableBody>
+              </Calendar.Table>
+            </Calendar.Context>
+          </Calendar.View>
+          <Calendar.View view="year">
+            <Calendar.Context v-slot="api">
+              <Calendar.ViewControl>
+                <Calendar.PrevTrigger>Prev</Calendar.PrevTrigger>
+                <Calendar.ViewTrigger>
+                  <Calendar.RangeText />
+                </Calendar.ViewTrigger>
+                <Calendar.NextTrigger>Next</Calendar.NextTrigger>
+              </Calendar.ViewControl>
+              <Calendar.Table>
+                <Calendar.TableBody>
+                  <Calendar.TableRow v-for="(years, id) in api.getYearsGrid({ columns: 4 })" :key="id">
+                    <Calendar.TableCell v-for="(year, id) in years" :key="id" :value="year.value">
+                      <Calendar.TableCellTrigger>{{ year.label }}</Calendar.TableCellTrigger>
+                    </Calendar.TableCell>
+                  </Calendar.TableRow>
+                </Calendar.TableBody>
+              </Calendar.Table>
+            </Calendar.Context>
+          </Calendar.View>
+        </Calendar.Content>
+      </Calendar.Positioner>
+    </Teleport>
+
   </Calendar.RootProvider>
 </template>
