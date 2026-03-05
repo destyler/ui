c6a3525cfa2bce2521847e8f36cfdf6eaa3d81b8
elonehoo <hi@elonehoo.me>
2026-03-05 18:13:09 +0800

    feat(carousel): add styles and structure for autoplay, basic, controlled, and root provider components

--- packages/vue/src/components/carousel/examples/Controlled.vue
+++ packages/vue/src/components/carousel/examples/Controlled.vue
@@ -7,19 +7,24 @@ const page = ref(0)
 </script>
 
 <template>
-  {{ page }}
-  <Carousel.Root v-model:page="page">
-    <Carousel.Control>
-      <Carousel.PrevTrigger>Previous</Carousel.PrevTrigger>
-      <Carousel.NextTrigger>Next</Carousel.NextTrigger>
-    </Carousel.Control>
-    <Carousel.IndicatorGroup>
-      <Carousel.Indicator v-for="(_, idx) in images" :key="idx" :index="idx" />
-    </Carousel.IndicatorGroup>
-    <Carousel.ItemGroup>
-      <Carousel.Item v-for="(image, idx) in images" :key="idx" :index="idx">
-        <img :src="image" alt="" />
-      </Carousel.Item>
-    </Carousel.ItemGroup>
-  </Carousel.Root>
+  <main>
+    <output>
+      {{ page }}
+    </output>
+    <Carousel.Root v-model:page="page">
+      <Carousel.Control>
+        <Carousel.PrevTrigger>Previous</Carousel.PrevTrigger>
+        <Carousel.NextTrigger>Next</Carousel.NextTrigger>
+      </Carousel.Control>
+      <Carousel.IndicatorGroup>
+        <Carousel.Indicator v-for="(_, idx) in images" :key="idx" :index="idx" />
+      </Carousel.IndicatorGroup>
+      <Carousel.ItemGroup>
+        <Carousel.Item v-for="(image, idx) in images" :key="idx" :index="idx">
+          <img :src="image" alt="" />
+        </Carousel.Item>
+      </Carousel.ItemGroup>
+    </Carousel.Root>
+  </main>
+
 </template>
--- packages/vue/src/components/carousel/examples/RootProvider.vue
+++ packages/vue/src/components/carousel/examples/RootProvider.vue
@@ -7,19 +7,21 @@ const carousel = useCarousel()
 </script>
 
 <template>
-  <button @click="carousel.scrollToIndex(2)">Scroll to #3</button>
-  <Carousel.RootProvider :value="carousel">
-    <Carousel.Control>
-      <Carousel.PrevTrigger>Previous</Carousel.PrevTrigger>
-      <Carousel.NextTrigger>Next</Carousel.NextTrigger>
-    </Carousel.Control>
-    <Carousel.IndicatorGroup>
-      <Carousel.Indicator v-for="(_, idx) in images" :key="idx" :index="idx" />
-    </Carousel.IndicatorGroup>
-    <Carousel.ItemGroup>
-      <Carousel.Item v-for="(image, idx) in images" :key="idx" :index="idx">
-        <img :src="image" alt="" />
-      </Carousel.Item>
-    </Carousel.ItemGroup>
-  </Carousel.RootProvider>
+  <main>
+    <button @click="carousel.scrollToIndex(2)">Scroll to #3</button>
+    <Carousel.RootProvider :value="carousel">
+      <Carousel.Control>
+        <Carousel.PrevTrigger>Previous</Carousel.PrevTrigger>
+        <Carousel.NextTrigger>Next</Carousel.NextTrigger>
+      </Carousel.Control>
+      <Carousel.IndicatorGroup>
+        <Carousel.Indicator v-for="(_, idx) in images" :key="idx" :index="idx" />
+      </Carousel.IndicatorGroup>
+      <Carousel.ItemGroup>
+        <Carousel.Item v-for="(image, idx) in images" :key="idx" :index="idx">
+          <img :src="image" alt="" />
+        </Carousel.Item>
+      </Carousel.ItemGroup>
+    </Carousel.RootProvider>
+  </main>
 </template>
