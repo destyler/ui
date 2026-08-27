98ea439cf76e36fb36fd42fb64b877429183b212
elonehoo <hi@elonehoo.me>
2026-08-27 09:49:00 +0000

    fix(fieldset): align Basic example and complete docs CSS
    Align React Basic to Vue (Field + invalid), fill empty basic/with-field
    demo styles, add checkbox hover/focus, soften WithCheckbox MDX copy,
    and register Vue stories for Storybook parity.

--- packages/react/src/components/fieldset/examples/Basic.tsx
+++ packages/react/src/components/fieldset/examples/Basic.tsx
@@ -1,13 +1,20 @@
+import { Field } from '../../field'
 import { Fieldset } from '../index'
 
 export interface BasicProps extends Fieldset.RootProps {}
 
 export function Basic(props: BasicProps) {
   return (
-    <Fieldset.Root {...props}>
+    <Fieldset.Root invalid {...props}>
       <Fieldset.Legend>Legend</Fieldset.Legend>
       <Fieldset.HelperText>Fieldset Helper Text</Fieldset.HelperText>
       <Fieldset.ErrorText>Fieldset Error Text</Fieldset.ErrorText>
+      <Field.Root>
+        <Field.Label>Label</Field.Label>
+        <Field.Input />
+        <Field.HelperText>Field Helper Text</Field.HelperText>
+        <Field.ErrorText>Field Error Text</Field.ErrorText>
+      </Field.Root>
     </Fieldset.Root>
   )
 }
